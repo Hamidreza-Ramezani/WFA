f5d3a9732a185b70da70190a6810db137cc53854
hamidreza-ramezani <hamid.ramezani1375@gmail.com>
2024-02-02 17:42:02 -0800

    minor

--- script.sh
+++ script.sh
@@ -12,8 +12,8 @@ l7=10000
 user="hamidkeb"
 #dataset_path="/home/$user/dataset"
 dataset_path="/data/hamid"
-pairs_count="5M"
-distance="e1"
+pairs_count="1M"
+distance="e5"
 data1="ERR240727"
 data2="SRR826460"
 data3="SRR826471"
@@ -34,39 +34,24 @@ data_path7="${dataset_path}/${synthetic}-l${l7}-${distance}-${pairs_count}Pairs"
 #./bin/align_benchmark -i ${data_path1} -a gap-affine-swg
 #./bin/align_benchmark -i ${data_path1} -a gap-affine-swg-banded --bandwidth 1
 #program="./bin/align_benchmark -i ${data_path1} -a gap-affine-wfa -g "-1,1,1,1"" 
-program="./bin/align_benchmark -i ${data_path2} -a gap-affine-wfa -g "-1,1,1,1""
+#program="./bin/align_benchmark -i ${data_path2} -a gap-affine-wfa -g "-1,1,1,1""
 #program="./bin/align_benchmark -i ${data_path3} -a gap-affine-wfa -g "-1,1,1,1""
 #program="./bin/align_benchmark -i ${data_path4} -a gap-affine-wfa -g "-1,1,1,1""
 #program="./bin/align_benchmark -i ${data_path5} -a gap-affine-wfa -g "-1,1,1,1""
 #program="./bin/align_benchmark -i ${data_path6} -a gap-affine-wfa-adaptive -g "-1,1,1,1""
-#program="./bin/align_benchmark -i ${data_path7} -a gap-affine-wfa-adaptive -g "-1,1,1,1""
+program="./bin/align_benchmark -i ${data_path7} -a gap-affine-wfa-adaptive -g "-1,1,1,1""
+
+#$program1
+#$program2
+#$program3
+#$program4
+#$program5
 
 
 start=`date +%s.%N`
 $program
-#valgrind --tool=cachegrind $program
-
 end=`date +%s.%N`
 echo "$end - $start" | bc -l 
 
-
-
-
-#prefix="/home/hamid/dataset/temp"
-#rm $prefix/x*
-#start=`date +%s.%N`
-#split -l 25000 -d "$prefix/synthetic-l5000-e1-1MPairs" "$prefix/x"
-#end=`date +%s.%N`
-##echo "$end - $start" | bc -l 
-##start=`date +%s.%N`
-#for i in $(seq -w 0 39); do
-#    filename="$prefix/x$i"
-#    #./bin/align_benchmark -i "$filename" -a gap-affine-wfa -g "-1,1,1,1" > /dev/null &
-#    ./bin/align_benchmark -i "$filename" -a gap-affine-wfa-adaptive -g "-1,1,1,1" > /dev/null 2>&1 &
-#    valgrind --tool=cachegrind ./bin/align_benchmark -i "$filename" -a gap-affine-wfa-adaptive > ./stat.txt
-#done
-#wait > /dev/null
-#end=`date +%s.%N`
-#echo "$end - $start" | bc -l 
 
 
