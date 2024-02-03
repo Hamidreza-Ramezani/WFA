432c170ade616ab904265cec90f1b54d63493933
Hamidreza Ramezani <hamidkeb@cortes.ece.ubc.ca>
2024-02-02 16:24:01 -0800

    new commit cortes

--- script.sh
+++ script.sh
@@ -9,10 +9,11 @@ l5=1000
 l6=5000
 l7=10000
 
-user="hamid"
-dataset_path="/home/$user/dataset"
-pairs_count="1M"
-distance="e5"
+user="hamidkeb"
+#dataset_path="/home/$user/dataset"
+dataset_path="/data/hamid"
+pairs_count="5M"
+distance="e1"
 data1="ERR240727"
 data2="SRR826460"
 data3="SRR826471"
@@ -33,12 +34,12 @@ data_path7="${dataset_path}/${synthetic}-l${l7}-${distance}-${pairs_count}Pairs"
 #./bin/align_benchmark -i ${data_path1} -a gap-affine-swg
 #./bin/align_benchmark -i ${data_path1} -a gap-affine-swg-banded --bandwidth 1
 #program="./bin/align_benchmark -i ${data_path1} -a gap-affine-wfa -g "-1,1,1,1"" 
-#program="./bin/align_benchmark -i ${data_path2} -a gap-affine-wfa -g "-1,1,1,1""
+program="./bin/align_benchmark -i ${data_path2} -a gap-affine-wfa -g "-1,1,1,1""
 #program="./bin/align_benchmark -i ${data_path3} -a gap-affine-wfa -g "-1,1,1,1""
 #program="./bin/align_benchmark -i ${data_path4} -a gap-affine-wfa -g "-1,1,1,1""
 #program="./bin/align_benchmark -i ${data_path5} -a gap-affine-wfa -g "-1,1,1,1""
 #program="./bin/align_benchmark -i ${data_path6} -a gap-affine-wfa-adaptive -g "-1,1,1,1""
-program="./bin/align_benchmark -i ${data_path7} -a gap-affine-wfa-adaptive -g "-1,1,1,1""
+#program="./bin/align_benchmark -i ${data_path7} -a gap-affine-wfa-adaptive -g "-1,1,1,1""
 
 
 start=`date +%s.%N`
