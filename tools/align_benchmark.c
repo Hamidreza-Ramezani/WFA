5039a7ef6eadd61fa20d94c50a8bff6ad25707ac
hamidreza-ramezani <hamid.ramezani1375@gmail.com>
2024-03-05 21:07:23 +0000

    important update: remove disk transfer from measurement

--- tools/align_benchmark.c
+++ tools/align_benchmark.c
@@ -49,12 +49,15 @@
 #include <pthread.h>
 #include <time.h>
 #include <sched.h>
+#include <string.h>
 
-#define MAX_LINE 10000000
+
+#define MAX_LINES 10000000
 
 #define NUM_THREADS 40
 
 long file_size;
+char **lines;
 
 /*
  * Algorithms
@@ -249,17 +252,17 @@ int moveFilePointerBackwardToBeginingOfLine(FILE *file) {
 void *align(void *args)
 {
     int thread_id = *(int *)args;
-    FILE *input_file = NULL;
+    //FILE *input_file = NULL;
     char *line1 = NULL, *line2 = NULL;
     int line1_length=0, line2_length=0;
-    size_t line1_size=0, line2_size=0;
+    //size_t line1_size=0, line2_size=0;
     align_input_t align_input;
     // Init
-    input_file = fopen(parameters.input, "r");
-    if (input_file==NULL) {
-      fprintf(stderr,"Input file '%s' couldn't be opened\n",parameters.input);
-      exit(1);
-    }
+    //input_file = fopen(parameters.input, "r");
+    //if (input_file==NULL) {
+    //  fprintf(stderr,"Input file '%s' couldn't be opened\n",parameters.input);
+    //  exit(1);
+    //}
     benchmark_align_input_clear(&align_input);
     align_input.debug_flags = 0;
     align_input.debug_flags |= parameters.check_metric;
@@ -272,37 +275,48 @@ void *align(void *args)
     align_input.verbose = parameters.verbose;
     align_input.mm_allocator = mm_allocator_new(BUFFER_SIZE_8M);
     // Read-align loop
-    int reads_processed = 0;
-    long bytes_per_thread = (file_size + NUM_THREADS - 1) / NUM_THREADS;
-    long start_byte = thread_id * bytes_per_thread;
-    long end_byte = (thread_id + 1) * bytes_per_thread - 1;
+    //int reads_processed = 0;
+    //long bytes_per_thread = (file_size + NUM_THREADS - 1) / NUM_THREADS;
+    //long start_byte = thread_id * bytes_per_thread;
+    //long end_byte = (thread_id + 1) * bytes_per_thread - 1;
 
     // If this is the last thread, include the remaining lines
+    //if (thread_id == NUM_THREADS - 1) {
+    //    end_byte = file_size;
+    //}
+    //long current_byte = start_byte;
+    //fseek(input_file, end_byte, SEEK_SET);
+    //if (moveFilePointerBackwardToBeginingOfLine(input_file) == 0) {
+    //    fseek(input_file, -1, SEEK_CUR);
+    //    moveFilePointerBackwardToBeginingOfLine(input_file);
+    //}
+    //end_byte = ftell(input_file);
+    //fseek(input_file, start_byte, SEEK_SET);
+    //if (moveFilePointerBackwardToBeginingOfLine(input_file) == 1) {
+    //    line1_length = getline(&line1, &line1_size, input_file);
+    //}
+    //start_byte = ftell(input_file);
+
+    //int num_lines = 10000000;
+    int lines_per_thread = (MAX_LINES + NUM_THREADS - 1) / NUM_THREADS;
+    int start_line = thread_id * lines_per_thread;
+    int end_line = (thread_id + 1) * lines_per_thread - 1;
     if (thread_id == NUM_THREADS - 1) {
-        end_byte = file_size;
+        end_line = MAX_LINES - 1;
     }
-    long current_byte = start_byte;
-    fseek(input_file, end_byte, SEEK_SET);
-    if (moveFilePointerBackwardToBeginingOfLine(input_file) == 0) {
-        fseek(input_file, -1, SEEK_CUR);
-        moveFilePointerBackwardToBeginingOfLine(input_file);
-    }
-    end_byte = ftell(input_file);
-    fseek(input_file, start_byte, SEEK_SET);
-    if (moveFilePointerBackwardToBeginingOfLine(input_file) == 1) {
-        line1_length = getline(&line1, &line1_size, input_file);
-    }
-    start_byte = ftell(input_file);
+    int current_line = start_line;
 
     //timer_reset(&align_input.timer);
     // Read the portion of the file
-    while (current_byte <= end_byte) {
+    while (current_line <= end_line) {
        // Read queries
-       line1_length = getline(&line1, &line1_size, input_file);
-       line2_length = getline(&line2, &line2_size, input_file);
-       if (line1_length == -1 || line2_length == -1) break;
-       // Configure input
-       //align_input.sequence_id = reads_processed;
+       //line1_length = getline(&line1, &line1_size, input_file);
+       //line2_length = getline(&line2, &line2_size, input_file);
+       //if (line1_length == -1 || line2_length == -1) break;
+       line1 = lines[current_line];
+       line2 = lines[current_line + 1];
+       line1_length = strlen(line1);
+       line2_length = strlen(line2);
        align_input.pattern = line1+1;
        align_input.pattern_length = line1_length-2;
        align_input.pattern[align_input.pattern_length] = '\0';
@@ -313,20 +327,20 @@ void *align(void *args)
            &align_input,&parameters.affine_penalties,
            parameters.min_wavefront_length,
            parameters.max_distance_threshold);
-       reads_processed += 1;
-       current_byte = ftell(input_file);
+       //reads_processed += 1;
+       current_line+=2;
     } //while
 
-    if (parameters.check_correct || parameters.check_score || parameters.check_alignments) {
-      benchmark_print_stats(stderr,&align_input,true);
-    }
+    //if (parameters.check_correct || parameters.check_score || parameters.check_alignments) {
+    //  benchmark_print_stats(stderr,&align_input,true);
+    //}
 
     //fprintf(stderr,"  => Time.Alignment    ");
     //timer_print(stderr,&align_input.timer,&parameters.timer_global);
-    fclose(input_file);
-    mm_allocator_delete(align_input.mm_allocator);
-    free(line1);
-    free(line2);
+    //fclose(input_file);
+    //mm_allocator_delete(align_input.mm_allocator);
+    //free(line1);
+    //free(line2);
     return NULL;
 }
 
@@ -337,8 +351,26 @@ void align_benchmark(const alg_algorithm_type alg_algorithm) {
 
     FILE *input_file = NULL;
     input_file = fopen(parameters.input, "r");
-    fseek(input_file, 0, SEEK_END);
-    file_size = ftell(input_file);
+    //fseek(input_file, 0, SEEK_END);
+    //file_size = ftell(input_file);
+    lines = malloc(MAX_LINES * sizeof(char *));
+    if (lines == NULL) {
+        perror("Memory allocation failed");
+        fclose(input_file);
+        return;
+    }
+
+    size_t len = 0;
+    ssize_t read;
+    char *line = NULL;
+    int count = 0;
+
+    while ((read = getline(&line, &len, input_file)) != -1) {
+        lines[count++] = line;
+        line = NULL; // getline will allocate a new buffer
+    }
+
+
     timer_restart(&(parameters.timer_global));
     // Create the threads
     for (int i = 0; i < NUM_THREADS; i++) {
@@ -353,6 +385,9 @@ void align_benchmark(const alg_algorithm_type alg_algorithm) {
 
     timer_stop(&(parameters.timer_global));
     timer_print(stderr,&parameters.timer_global,NULL);
+
+    free(lines); // Free the array of pointers
+    fclose(input_file);
     // Print the word count
 }
 
